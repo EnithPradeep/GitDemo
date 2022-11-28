0ce8945da3775e1da4ff2ddf34da66c34b7d4fb4
EnithPradeep <enithpradeep619@gmail.com>
2022-11-28 13:12:27 +0530

    Add three methods first time

---
+++ src/test/java/org/cls/SecondarySchool.java
@@ -0,0 +1,13 @@
+package org.cls;
+
+public class SecondarySchool {
+	public void Tamil() {
+		System.out.println("Tamil Subject");
+	}
+	public void English() {
+		System.out.println("English Subject");
+	}
+	public void Maths() {
+		System.out.println("Maths Subject");
+	}
+}
